68c41a1457c9f159b445c48f031b2d468c42c1c5
Raunak Mishra <raunakm725@gmail.com>
2024-02-26 01:41:23 +0530

    learnt about variables

---
+++ variables.js
@@ -0,0 +1,12 @@
+name = "Tony Stark";
+age = 50;
+price = 77.77;
+x = null;
+y = undefined;
+isOk = true;
+console.log(name);
+console.log(age);
+console.log(price);
+console.log(x);
+console.log(y);
+console.log(isOk);
