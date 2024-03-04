ebd9fa663fff6c93a8f3a879505571399bc0732b
Raunak Mishra <raunakm725@gmail.com>
2024-03-04 21:16:21 +0530

    learnt about let,const,var data-types

---
+++ dataType.js
@@ -0,0 +1,15 @@
+let fullName = "Raunak";
+console.log(fullName);
+let age=28;
+console.log(age);
+let totalPrice=99.95;
+console.log(totalPrice);
+
+var x=14.2;
+console.log(x);
+
+const y = "Sonoma";
+console.log(y);
+
+/*We would never use 'var' in Javascript because in 2015 ES6 or EcmaScript 6
+was released and it introduced let and const.*/
