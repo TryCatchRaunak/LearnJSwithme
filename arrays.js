fec67ed6c24aa5585693f18d8bce92f073fef5f1
Raunak Mishra <69429073+TryCatchRaunak@users.noreply.github.com>
2024-07-04 17:20:22 +0530

    Learnt how stoiring data in the form of objects is not as convinient as in arrays, then learnt looping of arrays which included for of , for each and for loop for now and also practised some questions

---
+++ arrays.js
@@ -0,0 +1,83 @@
+//how can we store marks of 5 students
+let marks_student1 = 97;
+let marks_student2 = 98;
+let marks_student3 = 99;
+let marks_student4 = 100;
+let marks_student5 = 96;
+//But it will be very inconvenient to store marks of 100 students like this.
+
+//Here for a soln we can create an object
+let markings={
+    std1: 97,
+    std2: 98,
+    std3: 99 ,   
+    std4: 100,
+    std5: 96
+}
+
+//But this method too is not too convinient as we have to write the marks of each student manually.
+
+//Here comes the concept of arrays into play 
+
+//array is a collection of elements of same type or different type in which data is stored linearly and accessed using index.
+
+let grades  = [97,98,99,100,96];
+console.log(grades);
+console.log(grades.length);
+
+
+
+//looping over arrays
+let heroes = ['superman','batman','spiderman','ironman','thor'];
+console.log(heroes[0]);
+console.log(heroes[1]);
+//but this method is repeatative and not convinient
+//here looping comes in
+
+//for loop
+for(let index=0;index<heroes.length;index++){
+    console.log(heroes[index]);
+}
+
+//for of loop
+for (let item of heroes){
+    console.log(item);
+}
+
+let cities=['Kolkata', 'Mumbai', 'Delhi', 'Chennai', 'Bangalore'];
+for(let city of cities){
+    console.log(city.toUpperCase());
+}
+
+//practice question1
+//given array of marks of students -> [85,97,44,37,76,60], find the avg of class
+let marks = [85,97,44,37,76,60];
+let total=0;
+let avg;
+for(i=0;i<marks.length;i++){
+    total= total + marks[i];
+    avg=total/marks.length;
+}
+console.log(avg);
+console.log(total);
+
+//alternative
+let marks1 = [85,97,44,37,76,60];
+let sum=0;
+for(let mark of marks1){
+    sum+=mark;
+}
+let avg1 = sum/marks1.length;
+console.log("The average of the class is: "+avg1);
+
+//practice question2
+/* for a given array with prices of 5 items = [250,645,300,900,50], all items have 10% discount on them. 
+ Find the final price after applying offer*/
+let prices = [250,645,300,900,50];
+let finalPrice=0;
+let idx=0;
+for (let price of prices){
+    price-=price*0.1;
+    finalPrice+=price;
+}
+console.log("The final price after applying offer is: "+finalPrice);
