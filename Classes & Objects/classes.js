b0379e334597ef39f5207581dc4bada30532d63a
Raunak Mishra <69429073+TryCatchRaunak@users.noreply.github.com>
2024-08-17 15:47:26 +0530

    Practised some questions on Classes Objects and Inheritance

--- Classes & Objects/classes.js
+++ Classes & Objects/classes.js
@@ -89,3 +89,47 @@ class Engineer extends Person{
 
 let raunak = new Engineer();
 /*If we have same fucntion in both parent and child class then the child class function will be called[METHOD OVERRIDING]*/
+
+
+
+
+
+
+
+
+//PRACTICE QUESTIONS
+
+/*Q.1. You are creating a website for your college. Create a class USER with two properties name and email.
+It also has a method called viewData() that allows user to view website data.*/
+class User{
+    constructor(name, email){
+        this.name = name;
+        this.email = email;
+    }
+
+    viewData(){
+        console.log(this.name);
+        console.log(this.email);
+    }
+}
+
+let user1 = new User("Raunak Mishra", "raunakm725@gmail.com");
+let user2 = new User("Rohit Verma", "rohitverma@gmail.com");
+let user3 = new User("Priya Yadav", "priyayadav@gmail.com");
+let user4 = new User("Rakesh Dvivedi", "rakhesdvivedi@yahoomail.com");
+
+
+
+/*Q.2. Create a new class called ADMIN which inherits from USER. Add a new method called editData() to ADMIN that allows
+it to edit website Data.*/
+class Admin extends User{
+    constructor(name, email){
+        super(name, email);
+    }
+
+    editData(){
+        console.log("Add some changes");
+    }
+}
+
+let admin1 = new Admin("Pankaj Tripathi", "pankajtripathi@gmail.com");
